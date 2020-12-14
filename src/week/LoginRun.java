be4a200bb827b0218cae335228b6820e8f4ddc2f
USER <USER@USER-PC>
2020-12-14 13:27:36 +0700

    initial commit

---
+++ src/week/LoginRun.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package week;
+
+import WebLogin.*;
+
+/**
+ *
+ * @author USER
+ */
+public class LoginRun {
+    public static void main(String[] args){
+        
+        LoginView lv = new LoginView();
+        lv.setLocationRelativeTo(null);
+        lv.setVisible(true);
+    }
+}
